a9503000df5f6174a25ac665325e6ede602119a1
hukuryo <ryohei.1998.05.09@gmail.com>
2024-05-24 21:06:38 +0900

    新しいエンドポイントを作成

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
 
-const app = new Hono()
+const app = new Hono();
+app.use("*", prettyJSON());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get("/entry/:id", (c) => {
+  const id = c.req.param("id");
+  return c.json({
+    "you id is": id,
+  });
+});
+
+export default app;
