26f09ba15dca26884ef4605b85753eb63d5f0d92
hukuryo <ryohei.1998.05.09@gmail.com>
2024-05-25 09:00:24 +0900

    PostリクエストのAPIを作成

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 
+let blogPosts = [
+  {
+    id: "1",
+    title: "blog1",
+    content: "content1"
+  },
+  {
+    id: "2",
+    title: "blog2",
+    content: "content2"
+  },
+  {
+    id: "3",
+    title: "blog3",
+    content: "content3"
+  }
+]
+
 const app = new Hono();
 app.use("*", prettyJSON());
 
@@ -14,5 +32,15 @@ app.get("/entry/:id", (c) => {
     "you id is": id,
   });
 });
+
+app.post("/post", async (c) => {
+  const { title, content } = await c.req.json<{
+    title: string;
+    content: string
+  }>();
+
+  const newPost = {id: String(blogPosts.length + 1), title, content};
+  blogPosts = [...blogPosts, newPost];
+})
 
 export default app;
