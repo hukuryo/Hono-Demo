7c39de7f1e9fc38492faebe1a869a36ef1d50678
hukuryo <ryohei.1998.05.09@gmail.com>
2024-10-08 23:33:03 +0900

    ZodをHonoを組み合わせてみた

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
+import { z, ZodError } from "zod";
 
 let blogPosts = [
-  {
-    id: "1",
-    title: "blog1",
-    content: "content1"
-  },
-  {
-    id: "2",
-    title: "blog2",
-    content: "content2"
-  },
-  {
-    id: "3",
-    title: "blog3",
-    content: "content3"
-  }
-]
+  { id: "1", title: "blog1", content: "content1" },
+  { id: "2", title: "blog2", content: "content2" },
+  { id: "3", title: "blog3", content: "content3" },
+];
 
 const app = new Hono();
 app.use("*", prettyJSON());
+
+const postSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  content: z.string().min(1, "Content is required"),
+});
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -29,33 +23,48 @@ app.get("/", (c) => {
 app.get("/entry/:id", (c) => {
   const id = c.req.param("id");
   return c.json({
-    "you id is": id,
+    "your id is": id,
   });
 });
 
 app.post("/post", async (c) => {
-  const { title, content } = await c.req.json<{
-    title: string;
-    content: string
-  }>();
+  try {
+    const parsedBody = postSchema.parse(await c.req.json());
 
-  const newPost = {id: String(blogPosts.length + 1), title, content};
-  blogPosts = [...blogPosts, newPost];
-})
+    const { title, content } = parsedBody;
+    const newPost = { id: String(blogPosts.length + 1), title, content };
+    blogPosts = [...blogPosts, newPost];
 
-app.put('/post/:id', async (c) => {
+    return c.json(newPost);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return c.json({ error: error.errors }, 400);
+    }
+    return c.json({ error: "An unknown error occurred" }, 500);
+  }
+});
+
+app.put("/post/:id", async (c) => {
   const id = c.req.param("id");
   const index = blogPosts.findIndex((p) => p.id === id);
 
-  if(index === -1){
-    return c.json({message: "Post not found"}, 400);
+  if (index === -1) {
+    return c.json({ message: "Post not found" }, 400);
   }
 
-  const {title, content} = await c.req.json();
+  try {
+    const parsedBody = postSchema.parse(await c.req.json());
 
-  blogPosts[index] = { ...blogPosts[index], title, content };
+    const { title, content } = parsedBody;
+    blogPosts[index] = { ...blogPosts[index], title, content };
 
-  return c.json(blogPosts[index]);
-})
+    return c.json(blogPosts[index]);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return c.json({ error: error.errors }, 400);
+    }
+    return c.json({ error: "An unknown error occurred" }, 500);
+  }
+});
 
 export default app;
